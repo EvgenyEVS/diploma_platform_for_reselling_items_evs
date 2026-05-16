10983193e02a62a221f3ca828dfa4892f74f1bef
EvgenyEVS <evis10@yandex.ru>
2026-05-16 20:19:27 +0300

    7

---
+++ src/main/java/ru/skypro/homework/model/Advertisements.java
@@ -0,0 +1,52 @@
+package ru.skypro.homework.model;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "ads")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = "author")
+@EqualsAndHashCode(of = "pk")
+
+
+public class Advertisements {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pk", nullable = false)
+    @Schema(description = "id объявления", example = "100")
+    private Integer pk;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
+    @Column(name = "title", nullable = false, length = 32)
+    @Size(min = 4, max = 32)
+    private String title;
+
+    @Column(name = "price", nullable = false)
+    @Min(0)
+    @Max(10000000)
+    @Schema(description = "цена объявления", example = "5000")
+    private Integer price;
+
+    @Column(name = "image")
+    @Schema(description = "ссылка на картинку объявления", example = "/images/ads/1.jpg")
+    private String image;
+
+    @Column(name = "description", nullable = false)
+    @Size(min = 8, max = 64)
+    @Schema(description = "описание объявления", example = "iPhone 100500")
+    private String description;
+
+}
---
+++ src/main/java/ru/skypro/homework/model/User.java
@@ -0,0 +1,57 @@
+package ru.skypro.homework.model;
+
+
+import lombok.*;
+import ru.skypro.homework.dto.Role;
+
+import javax.persistence.*;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = "adv")
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    @Size(min = 4, max = 32)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    @Size(min = 8, max = 16)
+    private String password;
+
+    @Column(name = "first_name", nullable = false)
+    @Size(min = 2, max = 16)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    @Size(min = 2, max = 16)
+    private String lastName;
+
+    @Column(name = "phone", nullable = false)
+    @Pattern(regexp = "\\+7\\s?\\(?\\d{3}\\)?\\s?\\d{3}-?\\d{2}-?\\d{2}")
+    private String phone;
+
+    @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "image", length = 255)
+    private String image;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Advertisements> ads = new HashSet<>();
+}
